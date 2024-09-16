701e524804211a3faa577fcaec004bb0ff650573
ydvicencio <yvicencio@hawkmail.hccfl.edu>
2024-09-16 13:35:26 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,56 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   //PersonalInfo
+
+  //Unit Test 1
+
+    string name = "Lucinda Potter";
+    string birthday = "6/24/1992";
+    string workPhone = "000-000-0101";
+    string cellPhone = "000-000-0189";
+
+    Console.WriteLine(name);
+    Console.WriteLine(birthday);
+    Console.WriteLine(workPhone);
+    Console.WriteLine(cellPhone);
+
+  //Unit Test 2
+
+    string name2 = "John Smith";
+    string birthday2 = "8/15/1978";
+    string workPhone2 = "000-111-0101";
+    string cellPhone2 = "000-111-0189";
+
+    Console.WriteLine(name2);
+    Console.WriteLine(birthday2);
+    Console.WriteLine(workPhone2);
+    Console.WriteLine(cellPhone2);
+
+  //Unit Test 3
+
+    string name3 = "Chris Johnson";
+    string birthday3 = "12/01/1987";
+    string workPhone3 = "000-222-0101";
+    string cellPhone3 = "000-222-0189";
+
+    Console.WriteLine(name3);
+    Console.WriteLine(birthday3);
+    Console.WriteLine(workPhone3);
+    Console.WriteLine(cellPhone3);
+
+  //Unit Test 4
+
+    string name4 = "Robert Hall";
+    string birthday4 = "2/27/1949";
+    string workPhone4 = "000-444-0101";
+    string cellPhone4 = "000-444-0189";
+
+    Console.WriteLine(name4);
+    Console.WriteLine(birthday4);
+    Console.WriteLine(workPhone4);
+    Console.WriteLine(cellPhone4);
+    
+    }
+}
